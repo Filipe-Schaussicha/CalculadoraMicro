3e01427822fbc56f8882ab5184745d336a0c8947
artur732 <silveiraartur777@gmail.com>
2026-05-14 22:50:25 -0300

    Add files via upload
    Botão de igual mostra a polonesa

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "./ui_mainwindow.h"
 
 #include <QDebug>
+#include "polonesa.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -9,17 +10,25 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
     this->equacao = new Equacao(ui->CbxInversao->isChecked());
+    //A
+    this->pctExpressao = new std::vector<pacoteDaExpressao>();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
     delete equacao;
+    //A
+    delete pctExpressao;
 }
 
 // Atualiza o mostrador da calculador
 void MainWindow::atualizaMostrador(){
     ui->LedMostrador->setText(equacao->paraString());
+}
+
+void MainWindow::meuAtualizaMostrador(QString txt){
+    ui->LedMostrador->setText(txt);
 }
 
 
@@ -31,6 +40,9 @@ void MainWindow::on_Btn0_clicked()
 {
     equacao->addOperando(NUM, "0");
     atualizaMostrador();
+
+    pacoteDaExpressao novo("0",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -38,6 +50,9 @@ void MainWindow::on_Btn1_clicked()
 {
     equacao->addOperando(NUM, "1");
     atualizaMostrador();
+
+    pacoteDaExpressao novo("1",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -45,6 +60,9 @@ void MainWindow::on_Btn2_clicked()
 {
     equacao->addOperando(NUM, "2");
     atualizaMostrador();
+
+    pacoteDaExpressao novo("2",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -52,6 +70,9 @@ void MainWindow::on_Btn3_clicked()
 {
     equacao->addOperando(NUM, "3");
     atualizaMostrador();
+
+    pacoteDaExpressao novo("3",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -59,6 +80,9 @@ void MainWindow::on_Btn4_clicked()
 {
     equacao->addOperando(NUM, "4");
     atualizaMostrador();
+
+    pacoteDaExpressao novo("4",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -66,6 +90,9 @@ void MainWindow::on_Btn5_clicked()
 {
     equacao->addOperando(NUM, "5");
     atualizaMostrador();
+
+    pacoteDaExpressao novo("5",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -73,6 +100,9 @@ void MainWindow::on_Btn6_clicked()
 {
     equacao->addOperando(NUM, "6");
     atualizaMostrador();
+
+    pacoteDaExpressao novo("6",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -80,6 +110,9 @@ void MainWindow::on_Btn7_clicked()
 {
     equacao->addOperando(NUM, "7");
     atualizaMostrador();
+
+    pacoteDaExpressao novo("7",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -87,6 +120,9 @@ void MainWindow::on_Btn8_clicked()
 {
     equacao->addOperando(NUM, "8");
     atualizaMostrador();
+
+    pacoteDaExpressao novo("8",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -94,6 +130,9 @@ void MainWindow::on_Btn9_clicked()
 {
     equacao->addOperando(NUM, "9");
     atualizaMostrador();
+
+    pacoteDaExpressao novo("9",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -101,6 +140,9 @@ void MainWindow::on_BtnVirgula_clicked()
 {
     equacao->addOperando(NUM, ",");
     atualizaMostrador();
+
+    pacoteDaExpressao novo(",",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -108,6 +150,9 @@ void MainWindow::on_BtnPi_clicked()
 {
     equacao->addOperando(PI, QString::number(3.14159265358979323846));
     atualizaMostrador();
+
+    pacoteDaExpressao novo("π",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -115,6 +160,10 @@ void MainWindow::on_BtnBackspace_clicked()
 {
     equacao->backspace();
     atualizaMostrador();
+
+    if(!pctExpressao->empty()){
+        pctExpressao->pop_back();
+    }
 }
 
 
@@ -123,6 +172,9 @@ void MainWindow::on_BtnClear_clicked()
     delete equacao;
     equacao = new Equacao(ui->CbxInversao->isChecked());
     atualizaMostrador();
+
+    delete pctExpressao;
+    pctExpressao = new std::vector<pacoteDaExpressao>();
 }
 
 
@@ -130,6 +182,9 @@ void MainWindow::on_BtnMais_clicked()
 {
     equacao->addOperador(SOMA);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("+",1,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -137,6 +192,9 @@ void MainWindow::on_BtnMenos_clicked()
 {
     equacao->addOperador(SUB);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("-",1,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -144,6 +202,9 @@ void MainWindow::on_BtnMult_clicked()
 {
     equacao->addOperador(MUL);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("*",2,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -151,6 +212,9 @@ void MainWindow::on_BtnDiv_clicked()
 {
     equacao->addOperador(DIV);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("/",2,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -159,6 +223,11 @@ void MainWindow::on_BtnLn_clicked()
     equacao->addLog(EULER, "");
     equacao->addOperador(ABRE_PARENTESES);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("ln",4,2.71828182845904523536);
+    pctExpressao->push_back(novo);
+    pacoteDaExpressao novo2("(",5,0);
+    pctExpressao->push_back(novo2);
 }
 
 
@@ -166,6 +235,9 @@ void MainWindow::on_BtnFat_clicked()
 {
     equacao->addOperador(FAT);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("!",4,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -174,6 +246,11 @@ void MainWindow::on_BtnSen_clicked()
     equacao->addOperador(SEN);
     equacao->addOperador(ABRE_PARENTESES);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("sen",4,0);
+    pctExpressao->push_back(novo);
+    pacoteDaExpressao novo2("(",5,0);
+    pctExpressao->push_back(novo2);
 }
 
 
@@ -182,6 +259,11 @@ void MainWindow::on_BtnCos_clicked()
     equacao->addOperador(COS);
     equacao->addOperador(ABRE_PARENTESES);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("cos",4,0);
+    pctExpressao->push_back(novo);
+    pacoteDaExpressao novo2("(",5,0);
+    pctExpressao->push_back(novo2);
 }
 
 
@@ -190,6 +272,11 @@ void MainWindow::on_BtnTg_clicked()
     equacao->addOperador(TG);
     equacao->addOperador(ABRE_PARENTESES);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("tan",4,0);
+    pctExpressao->push_back(novo);
+    pacoteDaExpressao novo2("(",5,0);
+    pctExpressao->push_back(novo2);
 }
 
 
@@ -198,6 +285,11 @@ void MainWindow::on_BtnRaizQuadrada_clicked()
     equacao->addRaiz(NUM, "2");
     equacao->addOperador(ABRE_PARENTESES);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("√",4,2);
+    pctExpressao->push_back(novo);
+    pacoteDaExpressao novo2("(",5,0);
+    pctExpressao->push_back(novo2);
 }
 
 
@@ -206,6 +298,9 @@ void MainWindow::on_BtnElevadoA2_clicked()
     equacao->addOperador(ELEVADO);
     equacao->addOperando(NUM, "2");
     atualizaMostrador();
+
+    pacoteDaExpressao novo("^2",3,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -214,6 +309,9 @@ void MainWindow::on_BtnEuler_clicked()
     equacao->addOperando(EULER, "2.71828182845904523536");
     equacao->addOperador(ELEVADO);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("e",0,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -221,6 +319,9 @@ void MainWindow::on_BtnAbreParenteses_clicked()
 {
     equacao->addOperador(ABRE_PARENTESES);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("(",5,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -228,6 +329,9 @@ void MainWindow::on_BntFechaParenteses_clicked()
 {
     equacao->addOperador(FECHA_PARENTESES);
     atualizaMostrador();
+
+    pacoteDaExpressao novo(")",5,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -235,6 +339,9 @@ void MainWindow::on_BtnFrac_clicked()
 {
     equacao->addOperador(UM_SOBRE_X);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("⁻¹",4,0);
+    pctExpressao->push_back(novo);
 }
 
 
@@ -242,12 +349,16 @@ void MainWindow::on_BtnElevado_clicked()
 {
     equacao->addOperador(ELEVADO);
     atualizaMostrador();
+
+    pacoteDaExpressao novo("^",3,0);
+    pctExpressao->push_back(novo);
 }
 
 
 void MainWindow::on_Btnlog_clicked()
 {
     DialogLerValor lerValor;
+
 
     lerValor.mudarLabel("Digite o valor da base do Log:");
 
@@ -256,10 +367,22 @@ void MainWindow::on_Btnlog_clicked()
 
         if(base == "e"){
             equacao->addLog(EULER, "");
+            pacoteDaExpressao novo("log",4,2.71828182845904523536);
+            pctExpressao->push_back(novo);
+            pacoteDaExpressao novo2("(",5,0);
+            pctExpressao->push_back(novo2);
         }else if(base == "π"){
             equacao->addLog(PI, "");
+            pacoteDaExpressao novo("log",4,3.14159265358979323846);
+            pctExpressao->push_back(novo);
+            pacoteDaExpressao novo2("(",5,0);
+            pctExpressao->push_back(novo2);
         }else{
             equacao->addLog(NUM, base);
+            pacoteDaExpressao novo("log",4,base.toFloat());
+            pctExpressao->push_back(novo);
+            pacoteDaExpressao novo2("(",5,0);
+            pctExpressao->push_back(novo2);
         }
 
         equacao->addOperador(ABRE_PARENTESES);
@@ -279,10 +402,22 @@ void MainWindow::on_BtnRaizY_clicked()
 
         if(base == "e"){
             equacao->addRaiz(EULER, "");
+            pacoteDaExpressao novo("√",4,2.71828182845904523536);
+            pctExpressao->push_back(novo);
+            pacoteDaExpressao novo2("(",5,0);
+            pctExpressao->push_back(novo2);
         }else if(base == "π"){
             equacao->addRaiz(PI, "");
+            pacoteDaExpressao novo("√",4,3.14159265358979323846);
+            pctExpressao->push_back(novo);
+            pacoteDaExpressao novo2("(",5,0);
+            pctExpressao->push_back(novo2);
         }else{
             equacao->addRaiz(NUM, base);
+            pacoteDaExpressao novo("√",4,base.toFloat());
+            pctExpressao->push_back(novo);
+            pacoteDaExpressao novo2("(",5,0);
+            pctExpressao->push_back(novo2);
         }
 
         equacao->addOperador(ABRE_PARENTESES);
@@ -302,5 +437,22 @@ void MainWindow::on_CbxInversao_checkStateChanged(const Qt::CheckState &arg1)
 {
     equacao->atualizarInversao(ui->CbxInversao->isChecked());
     atualizaMostrador();
+}
+
+
+void MainWindow::on_BtnIgual_clicked()
+{
+    pacoteDaExpressao pct;
+
+    polonesa portalPolones;
+
+    std::vector<pacoteDaExpressao> temporario = portalPolones.polonese(*pctExpressao);
+
+
+    if(temporario[0].getProcedencia()>=0) {
+        meuAtualizaMostrador(pct.transformaEmTexto(temporario));
+    } else {
+        meuAtualizaMostrador("Erro de sintaxe!");
+    }
 }
 
